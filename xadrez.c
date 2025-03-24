2412923af2b58a053cad8236deb6dc75ada795ba
Lucas Valim <lucasvalim17@gmail.com>
2025-03-24 14:42:49 +0000

    Feito o código da lógica de Loops Aninhados para a movimentação do CAVALO.

--- xadrez.c
+++ xadrez.c
@@ -2,13 +2,16 @@
 
 int main() {
     
-    int torre;
+    int torre, i;
     int bispo = 1;
     int rainha = 1;
+    int j = 1;
+
 
     //Movimentação da TORRE:
 
     printf("Torre:\n");
+
     for(torre = 1; torre <= 5; torre++) {
         printf("Andar %d casa para a direta!\n", torre);
     }
@@ -20,9 +23,12 @@ int main() {
     printf("Bispo:\n");
 
     do {
+
         printf("Ande %d casa para cima e para a direita!\n", bispo);
         bispo++;
+        
     }
+
     while(bispo <= 5);
 
     printf("\n");
@@ -30,9 +36,26 @@ int main() {
     //Movimentação da RAINHA:
 
     printf("Rainha:\n");
+
     while(rainha <= 8) {
         printf("Ande %d para a esquerda!\n", rainha);
         rainha++;
+    }
+
+    printf("\n");
+
+    //Movimentação do CAVALO:
+
+    printf("Cavalo:\n");
+
+    for(i = 1; i <= 1; i++) {
+
+        while(j <= 2) {
+            printf("%d Casa para baixo\n", j);
+            j++;
+        }
+        printf("%d Casa para a esquerda", i);
+        printf("\n");
     }
 
     return 0;
